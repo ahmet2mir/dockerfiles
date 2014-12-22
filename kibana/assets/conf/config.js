9e43995b6f8dfaf40b74653ca73e767f6860ede4
ahmet2mir <ahmet2mir+github@gmail.com>
2014-12-22 12:20:50 +0100

    Add Kibana

---
+++ kibana/assets/conf/config.js
@@ -0,0 +1,25 @@
+define(['settings'],
+function (Settings) {
+  return new Settings({
+    elasticsearch: "{{ ES_SCHEME }}://{{ ES_HOST }}:{{ ES_PORT }}",
+    default_route: '/dashboard/file/logstash.json',
+    kibana_index: "kibana-int",
+    panel_names: [
+      'histogram',
+      'map',
+      'goal',
+      'table',
+      'filtering',
+      'timepicker',
+      'text',
+      'hits',
+      'column',
+      'trends',
+      'bettermap',
+      'query',
+      'terms',
+      'stats',
+      'sparklines'
+    ]
+  });
+});
---
+++ kibana/assets/scripts/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ES_HOST=${ES_HOST:-\"+window.location.hostname+\"}
+ES_PORT=${ES_PORT:-9200}
+ES_SCHEME=${ES_SCHEME:-http}
+
+# If the container stop, don't run this part
+# docker start will run this script
+if [ ! -f /.docker_status ]
+then
+
+	sed 's,{{ES_HOST}},'"${ES_HOST}"',g' -i /webapps/kibana/config.js
+	sed 's,{{ES_PORT}},'"${ES_PORT}"',g' -i /webapps/kibana/config.js
+	sed 's,{{ES_SCHEME}},'"${ES_SCHEME}"',g' -i /webapps/kibana/config.js
+
+    echo "************ Create status file to be idempotent"
+    echo "done" > /.docker_status
+fi
+
+echo "************ Finished. Running Nginx in FOREGROUND"
+exec nginx -c /etc/nginx/nginx.conf
+
