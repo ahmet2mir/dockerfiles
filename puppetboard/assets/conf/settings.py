da16b7e6823af24a6a6b5f27d887bcd63d73bab0
ahmet2mir <ahmet2mir+github@gmail.com>
2014-12-29 16:47:34 +0100

    edit settings

--- puppetboard/assets/conf/settings.py
+++ puppetboard/assets/conf/settings.py
@@ -1,11 +1,14 @@
-LOGLEVEL = 'info'
-PUPPETDB_HOST = str("{{ PUPPETDB_HOST }}")
-PUPPETDB_PORT = int("{{ PUPPETDB_PORT }}")
-PUPPETDB_SSL_VERIFY = bool("{{ PUPPETDB_VERIFY }}")
+def str2bool(value):
+	return value.lower() in ("true", "yes", "on", "1")
 
-if PUPPETDB_SSL_VERIFY:
-    PUPPETDB_KEY = "/webapps/puppetboard/ssl/puppetboard.pem"
-    PUPPETDB_CERT = "/webapps/puppetboard/ssl/puppetboard.crt"
+LOGLEVEL = 'info'
+PUPPETDB_HOST = "{{ PUPPETDB_HOST }}"
+PUPPETDB_PORT = int("{{ PUPPETDB_PORT }}")
+PUPPETDB_SSL_VERIFY = str2bool("{{ PUPPETDB_VERIFY }}")
+
+if not "localhost" in PUPPETDB_HOST:
+	PUPPETDB_KEY = "/webapps/puppetboard/ssl/puppetboard.pem"
+	PUPPETDB_CERT = "/webapps/puppetboard/ssl/puppetboard.crt"
 
 PUPPETDB_TIMEOUT = 20
 UNRESPONSIVE_HOURS = 3
